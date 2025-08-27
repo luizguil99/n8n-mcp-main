56886ef19bbfd168c3cbb41d9dd9ffbcc2bcd8c6
github-action-benchmark <github@users.noreply.github.com>
2025-08-27 20:59:40 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for aab84acbfbd53efbdef9a4691a0ff3d6f7eb8bc5

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1756328380117,
+  "repoUrl": "https://github.com/luizguil99/n8n-mcp-main",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "124329344+luizguil99@users.noreply.github.com",
+            "name": "luizguil99",
+            "username": "luizguil99"
+          },
+          "committer": {
+            "email": "124329344+luizguil99@users.noreply.github.com",
+            "name": "luizguil99",
+            "username": "luizguil99"
+          },
+          "distinct": true,
+          "id": "aab84acbfbd53efbdef9a4691a0ff3d6f7eb8bc5",
+          "message": "update",
+          "timestamp": "2025-08-27T17:57:42-03:00",
+          "tree_id": "ffc2d5af2a53e31b5507c6ae3426f4a4007a0a27",
+          "url": "https://github.com/luizguil99/n8n-mcp-main/commit/aab84acbfbd53efbdef9a4691a0ff3d6f7eb8bc5"
+        },
+        "date": 1756328379270,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0189,
+            "unit": "ms",
+            "range": 0.27849999999999997,
+            "extra": "52793 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1287,
+            "unit": "ms",
+            "range": 0.4373,
+            "extra": "320 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0048,
+            "unit": "ms",
+            "range": 0.26180000000000003,
+            "extra": "208227 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0658,
+            "unit": "ms",
+            "range": 0.31880000000000003,
+            "extra": "15198 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
